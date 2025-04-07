b5d78b71a76795ce520c8c448f7876d4480f76f9
ender320 <vinicius.gamed@gmail.com>
2025-04-07 12:50:53 -0300

    Sistema de Animação
    The character sprite placeholder is made by Game Endeavor on Itch.io
    
    Nesse prototipo foi adicionado sistema de animação e algumas correçõeszinhas no código.

--- enemy.gd
+++ enemy.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-var speed = 80
+var speed = 60
 var agressive_state = false #idle
 var player 
 var dialogic_start = true
@@ -7,7 +7,10 @@ var dialogic_start = true
 func _physics_process(delta):
 	if agressive_state == true: #isso aqui vai fazer com que o noll vá átras do player!
 		position += (player.position - position)/speed #isso aqui faz seguir
-	
+		$detec_zone/MeshInstance2D.visible = false
+		play_anim(1)
+	if agressive_state == false:
+		play_anim(0)
 func _on_detec_zone_body_entered(body):
 	player = body #player vai ser o corpo que vai entrar dentro da area de colisão
 	if dialogic_start == true:
@@ -27,3 +30,10 @@ func _on_hitbox_area_entered(area):
 func _on_detec_zone_body_exited(body):
 	player = body
 	agressive_state = false
+
+func play_anim(movement):
+	var anim = $AnimatedSprite_enemy
+	if movement == 1:
+		anim.play("agressivo")
+	elif movement == 0:
+		anim.play("idle")
--- player.gd
+++ player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-var speed = 400
+var speed = 300
 var vida = 1
+var current_dir = "none"
 
 func _ready():
+	$AnimatedSprite2D.play("idle_cima")
 	if Dialogic.VAR.intro == true:
 		Dialogic.start("dialogo_introdução")
 	
@@ -12,14 +14,14 @@ func _process(delta):
 	if Dialogic.VAR.lendo == true:
 		speed = 0
 	else:
-		speed = 400
+		speed = 300
 	if vida == 0:
 		get_tree().reload_current_scene()
 
 func _physics_process(delta):
 	movimentação_vertical()
 	#movimentação_top_down()
-
+	move_and_slide()
 
 func _on_hurtbox_area_entered(area):
 	if area.name == "hitbox":
@@ -27,23 +29,29 @@ func _on_hurtbox_area_entered(area):
 
 func movimentação_top_down():
 	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
 		velocity.y = -speed
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
 		velocity.y = speed
 		velocity.x = 0
 	else:
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
-	
-	move_and_slide()
-	pass
 
 func movimentação_vertical():
 	var direção = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
@@ -51,4 +59,48 @@ func movimentação_vertical():
 		velocity = direção * speed
 	else:
 		velocity = Vector2.ZERO
-	move_and_slide()
+	
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+	elif  Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+	else:
+		play_anim(0)
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("andando_lado")
+		elif movement == 0:
+			anim.play("idle_lado")
+
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("andando_lado")
+		elif movement == 0:
+			anim.play("idle_lado")
+
+	if dir == "up":
+		if movement == 1:
+			anim.play("andando_cima")
+		elif movement == 0:
+			anim.play("idle_cima")
+
+	if dir == "down":
+		if movement == 1:
+			anim.play("andando_baixo")
+		elif movement == 0:
+			anim.play("idle_baixo")
+	
