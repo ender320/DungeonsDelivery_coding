25ecbcc1a5e642d82cf21131ba5b3b6a1376700b
ender320 <vinicius.gamed@gmail.com>
2025-04-08 21:52:06 -0300

    prototipo_inicial

--- mundo_2_switch.gd
+++ mundo_2_switch.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "player":
-		get_tree().change_scene_to_file("res://mundo_2.tscn")
+		get_tree().change_scene_to_file("res://Cenas/mundo_2.tscn")
--- mundo_3_switch.gd
+++ mundo_3_switch.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "player":
-		get_tree().change_scene_to_file("res://mundo_3.tscn")
+		get_tree().change_scene_to_file("res://Cenas/mundo_3.tscn")
